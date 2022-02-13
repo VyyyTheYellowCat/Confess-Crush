591eec6f642e44006ba3088d967d4a82f2423707
VyyyTheYellowCat <99161542+VyyyTheYellowCat@users.noreply.github.com>
2022-02-13 08:20:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    titleWeb: "Hiiii :D",
+    introTitle: 'xin chào cậu người ở trước màn hình 😏💅',
+    introDesc: `Nụ cười của Super Idol
+Cũng không ngọt như cậu
+Cái nắng chói chang giữa tháng 8
+Cũng không tỏa nắng bằng cậu =))`,
+    btnIntro: ':D',
+    title: 'Chọn nút thích hay ko thích đi nha tr',
+    desc: 'Nhớ chọn nhanh á ',
+    btnYes: 'Thích Vy lắm lun á',
+    btnNo: 'Không thích Vy ,-,',
+    question: 'Tại sao cậu thích tớ 😼',
+    btnReply: 'Gửi cho Vy ',
+    reply: 'Tại Vy đẹp try 😼💅🏻',
+    mess: 'omg  =))',
+    messDesc: 'ăn ốc quế sầu riêng cùng với tớ ko 🙀?.',
+    btnAccept: 'Okiiii',
+    messLink: 'https://www.google.com/search?q=%E1%BB%91c+qu%E1%BA%BF+s%E1%BA%A7u+ri%C3%AAng&sxsrf=APq-WBuCWOfqgbDsG2Sd-MXfjn5FB-uqwQ:1644236871965&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiT77fVy-31AhWUVPUHHcbrAEsQ_AUoAXoECAEQAw&biw=1366&bih=657&dpr=1#imgrc=-zgcabiycmgsiM' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
