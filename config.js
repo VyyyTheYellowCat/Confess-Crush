1852c95b9e4a6c8e61f59f00c851aa8c7997892c
Vyyy <99161542+VyyyTheYellowCat@users.noreply.github.com>
2023-01-14 20:52:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
     titleWeb: "Hiiii :D",
-    introTitle: 'xin chào cậu người ở trước màn hình 😏💅',
-    introDesc: `Nụ cười của Super Idol
-Cũng không ngọt như cậu
-Cái nắng chói chang giữa tháng 8
-Cũng không tỏa nắng bằng cậu =))`,
+    introTitle: 'Heloooo Thảo Nhi nhe :>',
+    introDesc: `=))`,
     btnIntro: ':D',
-    title: 'Chọn nút thích hay ko thích đi nha tr',
+    title: 'Có thích quà sinh nhật hok Nhi>?',
     desc: 'Nhớ chọn nhanh á ',
-    btnYes: 'Thích Vy lắm lun á',
-    btnNo: 'Không thích Vy ,-,',
-    question: 'Tại sao cậu thích tớ 😼',
+    btnYes: 'Có',
+    btnNo: 'Khogggg,-,',
+    question: 'Tại sao bà thích quà sinh nhật 😼',
     btnReply: 'Gửi cho Vy ',
-    reply: 'Tại Vy đẹp try 😼💅🏻',
+    reply: 'tại vì quà sinh nhật của bà ý nghĩa mà🏻',
     mess: 'omg  =))',
-    messDesc: 'ăn ốc quế sầu riêng cùng với tớ ko 🙀?.',
-    btnAccept: 'Okiiii',
+    messDesc: 'ăn ốc quế sầu riêngvới tui khog 🙀?.',
+    btnAccept: 'Yeeeee!',
     messLink: 'https://www.google.com/search?q=%E1%BB%91c+qu%E1%BA%BF+s%E1%BA%A7u+ri%C3%AAng&sxsrf=APq-WBuCWOfqgbDsG2Sd-MXfjn5FB-uqwQ:1644236871965&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiT77fVy-31AhWUVPUHHcbrAEsQ_AUoAXoECAEQAw&biw=1366&bih=657&dpr=1#imgrc=-zgcabiycmgsiM' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
